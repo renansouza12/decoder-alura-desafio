519fdcb0f0332fbc4058ce9086463ac25127815f
RenanSouza <renansouza0122@gmail.com>
2024-02-03 15:30:46 -0300

    option to rotate cards added

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,3 @@
-
-
 const btnsMinimize = document.querySelectorAll(".minimize");
 const btnsMaximaize = document.querySelectorAll(".maximize");
 const btnsClose = document.querySelectorAll(".close");
@@ -12,45 +10,47 @@ const decodeIcone = document.querySelector(".decode_icon");
 const warningIcone = document.querySelector(".warning_icon");
 const decryptIcone = document.querySelector(".decrypt_icon");
 
+const rotateIcone = document.querySelector(".rotate_icon");
+
 const minimizeBarDecode = document.querySelector('.minimized_bar_decode');
 const minimizeBarDecrypt = document.querySelector('.minimized_bar_decrypt');
 const minizeBarWarning = document.querySelector('.minimized_bar_warning');
 
-btnsMinimize.forEach((btnMinimize,i)=>{
-    btnMinimize.addEventListener("click", ()=>{
-        i == 0 ?  (minimizeDiv(decoderCard),displayIcone(decodeIcone), addClass(minimizeBarDecode, 'minimized')) : 
-        i == 1 ?  (minimizeDiv(decryptedCard),displayIcone(decryptIcone), addClass(minimizeBarDecrypt, 'minimized')) :
-        i == 2 ? (minimizeDiv(warningCard),displayIcone(warningIcone), addClass(minizeBarWarning, 'minimized')) : '';
+btnsMinimize.forEach((btnMinimize, i) => {
+    btnMinimize.addEventListener("click", () => {
+        i == 0 ? (minimizeDiv(decoderCard), displayIcone(decodeIcone), addClass(minimizeBarDecode, 'minimized')) :
+            i == 1 ? (minimizeDiv(decryptedCard), displayIcone(decryptIcone), addClass(minimizeBarDecrypt, 'minimized')) :
+                i == 2 ? (minimizeDiv(warningCard), displayIcone(warningIcone), addClass(minizeBarWarning, 'minimized')) : '';
     })
 })
-btnsMaximaize.forEach((btnMaximaize,i)=>{
-    btnMaximaize.addEventListener("click", ()=>{
-       i == 0 ?  maximizeDiv(decoderCard) : i == 1 ?  maximizeDiv(decryptedCard) : i == 2 ? maximizeDiv(warningCard) : '';
+btnsMaximaize.forEach((btnMaximaize, i) => {
+    btnMaximaize.addEventListener("click", () => {
+        i == 0 ? maximizeDiv(decoderCard) : i == 1 ? maximizeDiv(decryptedCard) : i == 2 ? maximizeDiv(warningCard) : '';
     })
 })
-btnsClose.forEach((btnClose,i)=>{
-    btnClose.addEventListener("click", ()=>{
-       i == 0 ?  (closeDiv(decoderCard), displayIcone(decodeIcone) ): 
-       i == 1 ?  (closeDiv(decryptedCard),displayIcone(decryptIcone)) :
-        i == 2 ? (closeDiv(warningCard),displayIcone(warningIcone) ): '';
+btnsClose.forEach((btnClose, i) => {
+    btnClose.addEventListener("click", () => {
+        i == 0 ? (closeDiv(decoderCard), displayIcone(decodeIcone)) :
+            i == 1 ? (closeDiv(decryptedCard), displayIcone(decryptIcone)) :
+                i == 2 ? (closeDiv(warningCard), displayIcone(warningIcone)) : '';
     })
 })
 
 warningIcone.addEventListener("click", () => displayCardMinimized(warningCard, minizeBarWarning));
 decodeIcone.addEventListener("click", () => displayCardMinimized(decoderCard, minimizeBarDecode));
 decryptIcone.addEventListener("click", () => displayCardMinimized(decryptedCard, minimizeBarDecrypt));
-
-function minimizeDiv(div){
+rotateIcone.addEventListener("click", () => rotateCard())
+function minimizeDiv(div) {
     div.style.display = "none";
 }
-function displayIcone(icon){
+function displayIcone(icon) {
     icon.style.display = 'block';
 }
 
-function maximizeDiv(div){
+function maximizeDiv(div) {
     div.classList.toggle('maximized');
 }
-function closeDiv(div){
+function closeDiv(div) {
     div.style.display = "none";
     decoderInput.value = "";
     result.value = "";
@@ -67,9 +67,16 @@ function displayCardMinimized(div, minimizeBar) {
     removeClass(minimizeBar, 'minimized');
 }
 
-    
+
 function removeClass(element, className) {
     if (element.classList.contains(className)) {
         element.classList.remove(className);
     }
+}
+
+function rotateCard() {
+    decoderCard.classList.toggle('rotated');
+    warningCard.classList.toggle('rotated');
+    decryptedCard.classList.toggle('rotated');
+    rotateIcone.classList.toggle('rotated');
 }
